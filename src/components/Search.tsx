95a566c9cae10e8a5faf4f97cd2875aa225b56bb
Gabriel Duarte <gabriel.gd7@outlook.com>
2023-01-07 18:10:12 -0300

    keyDownEvent

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,15 +3,23 @@ type SearchProps = {
 }
 
 import {BsSearch} from 'react-icons/bs';
+import {useState, KeyboardEvent} from 'react';
+import styles from './Search.module.css';
 
 const Search = ({loadUser}:SearchProps) => {
+  const [userName, setUserName] = useState<string>("");
+  const handleKeyDown = (e:KeyboardEvent):void =>{
+    if (e.key === "Enter")
+      loadUser(userName);
+
+  }
   return (
-    <div>
+    <div className={styles.search}>
       <h2>Busque por um usuario:</h2>
       <p>Conheça seus melhores repositórios</p>
-      <div>
-        <input type="text" placeholder='Digite o nome do usuario' />
-        <button>
+      <div className={styles.searchContainer}>
+        <input type="text" placeholder='Digite o nome do usuario' onChange={e=>setUserName(e.target.value)} onKeyDown={handleKeyDown} />
+        <button onClick={()=>loadUser(userName)}>
           <BsSearch/> 
         </button>
       </div>
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -8,16 +8,26 @@ const Home = () => {
   const [user,setUser] = useState<UserProps | null>(null);
 
   const loadUser = async (userName:string) : Promise<void> =>{
-    const res = await fetch(`https://api.github/users/${userName}`);
+    const res = await fetch(`https://api.github.com/users/${userName}`);
 
     const data = await res.json();
 
-    console.log(data);
+    const {avatar_url, following, followers, location, login} = data;
+
+    const objUser: UserProps = {
+      avatar_url,
+      followers,
+      following,
+      location,
+      login
+    }
+    setUser(objUser);
   }
 
   return (
     <div>
       <Search loadUser={loadUser}/>
+      {user && <p>{user.login}</p>}
     </div>
   )
 }
